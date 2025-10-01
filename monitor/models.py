b373df18d9a39b7779244ca71cea3fb32c7ab92d
Everton-Renan <evertonrenan917@gmail.com>
2025-10-01 15:44:12 -0300

    Add initial migration and models for Server and CheckLog

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class CheckLog(models.Model):
+    class Meta:
+        verbose_name_plural = "Check Logs"
+
+    server = models.ForeignKey(Server, on_delete=models.CASCADE, editable=False)
+    status_code = models.IntegerField(editable=False)
+    response_time = models.FloatField(editable=False)
+    checked_at = models.DateTimeField(auto_now_add=True, editable=False)
