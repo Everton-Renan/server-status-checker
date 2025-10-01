ed399c87889dba7ebc43611080b6edf5fb27217b
Everton-Renan <evertonrenan917@gmail.com>
2025-10-01 15:44:23 -0300

    Register Server and CheckLog models in the Django admin

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import CheckLog, Server
+
+
 # Register your models here.
+@admin.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url", "is_active")
+    list_display_links = ("id", "name", "url", "is_active")
+    search_fields = ("id", "name", "url")
+
+
+@admin.register(CheckLog)
+class CheckLogAdmin(admin.ModelAdmin):
+    list_display = ("id", "server", "status_code", "response_time", "checked_at")
+    list_display_links = ("id", "server", "status_code", "response_time", "checked_at")
+    search_fields = ("id", "server")
