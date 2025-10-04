0f0bcce8b4678c88cf0fe63831412f1d33b54b53
Everton-Renan <evertonrenan917@gmail.com>
2025-10-04 00:37:00 -0300

    Remove editable fields in CheckLog and add error field, and Create Settings model

--- monitor/models.py
+++ monitor/models.py
@@ -15,7 +15,21 @@ class CheckLog(models.Model):
     class Meta:
         verbose_name_plural = "Check Logs"
 
-    server = models.ForeignKey(Server, on_delete=models.CASCADE, editable=False)
-    status_code = models.IntegerField(editable=False)
-    response_time = models.FloatField(editable=False)
-    checked_at = models.DateTimeField(auto_now_add=True, editable=False)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    status_code = models.IntegerField()
+    response_time = models.FloatField()
+    error = models.CharField(default="")
+    checked_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.server.name
+
+
+class Settings(models.Model):
+    class Meta:
+        verbose_name_plural = "Settings"
+
+    check_interval_seconds = models.PositiveIntegerField(
+        default=300,
+        help_text="It will be necessary to restart the application for the new interval to take effect.",
+    )
