f315ceb99c0f5f76b4266857b057b4089f9cbb55
Everton-Renan <evertonrenan917@gmail.com>
2025-10-04 00:37:30 -0300

    Register Settings model in Django admin and update CheckLog admin configuration

--- monitor/admin.py
+++ monitor/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CheckLog, Server
+from .models import CheckLog, Server, Settings
 
 
 # Register your models here.
@@ -13,6 +13,35 @@ class ServerAdmin(admin.ModelAdmin):
 
 @admin.register(CheckLog)
 class CheckLogAdmin(admin.ModelAdmin):
-    list_display = ("id", "server", "status_code", "response_time", "checked_at")
-    list_display_links = ("id", "server", "status_code", "response_time", "checked_at")
+    readonly_fields = (
+        "id",
+        "server",
+        "status_code",
+        "error",
+        "response_time",
+        "checked_at",
+    )
+    list_display = (
+        "id",
+        "server",
+        "status_code",
+        "error",
+        "response_time",
+        "checked_at",
+    )
+    list_display_links = (
+        "id",
+        "server",
+        "status_code",
+        "error",
+        "response_time",
+        "checked_at",
+    )
     search_fields = ("id", "server")
+
+
+@admin.register(Settings)
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ("id", "check_interval_seconds")
+    list_display_links = ("id", "check_interval_seconds")
+    search_fields = ("id",)
