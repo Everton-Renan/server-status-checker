524f8b763dc74baf0bc0a9a781016f816bee7deb
Everton-Renan <evertonrenan917@gmail.com>
2025-10-04 00:38:22 -0300

    Implement background scheduler to log server status at defined intervals

--- monitor/apps.py
+++ monitor/apps.py
@@ -1,6 +1,28 @@
+import os
+
+from apscheduler.schedulers.background import BackgroundScheduler
 from django.apps import AppConfig
 
 
 class MonitorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'monitor'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "monitor"
+
+    def ready(self) -> None:
+        if os.environ.get("RUN_MAIN", None) != "true":
+            return
+
+        from .models import Settings
+        from .tasks import create_log
+
+        check_interval = Settings.objects.first()
+
+        scheduler = BackgroundScheduler()
+
+        if check_interval is None:
+            scheduler.add_job(create_log, "interval", seconds=300)
+        else:
+            scheduler.add_job(
+                create_log, "interval", seconds=check_interval.check_interval_seconds
+            )
+        scheduler.start()
